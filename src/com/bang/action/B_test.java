8629c65424899bb5e8ea61fb7445d7d0abebb44e
tonsr <ak48159@qq.com>
2015-03-07 11:18:24 +0800

    Update B_test.java

--- src/com/bang/action/B_test.java
+++ src/com/bang/action/B_test.java
@@ -4,9 +4,6 @@ public class B_test {
 	
 	public static void main(String[] args) {
 		System.out.print("xxxxxxxxxx");
-		System.out.print("xxxxxxxxxx");
-		System.out.print("ytfkjhjbjhb");
-		System.out.print("jbikjnknk");
 	}
 
 }
