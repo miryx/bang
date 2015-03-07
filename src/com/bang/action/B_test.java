e7a0819edb90af5572dc939fd560673636a4db6f
Administrator <Administrator@VZDM85BYFVNLIN3>
2015-03-07 10:59:40 +0800

    yyyyr

--- src/com/bang/action/B_test.java
+++ src/com/bang/action/B_test.java
@@ -5,7 +5,7 @@ public class B_test {
 	public static void main(String[] args) {
 		System.out.print("xxxxxxxxxx");
 		System.out.print("xxxxxxxxxx");
-		System.out.print("xxxxxxxxxx");
+		System.out.print("ytfkjhjbjhb");
 		System.out.print("jbikjnknk");
 	}
 
